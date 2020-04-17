2fab2590e651dd827f9ca97fcdcfc7cb9d99575e
Henrique Starling <henrique_st@yahoo.com.br>
2020-04-17 18:48:39 -0300

    rails new

--- db/migrate/20200417205642_add_address_to_restaurante.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToRestaurante < ActiveRecord::Migration[6.0]
-  def change
-    add_column :restaurantes, :address, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_205335) do
+ActiveRecord::Schema.define(version: 2020_04_17_205642) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
